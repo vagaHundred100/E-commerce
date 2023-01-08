6e6d13d682aecb12e3c8c9c7b0f3c2d8a63fb931
TuralRehimov1998@outlook.com <TuralRehimov1998@outlook.com>
2023-01-08 04:47:01 +0400

    Adding view page

---
+++ E-CommerceApp/Controllers/SiteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_CommerceApp.Controllers
+{
+    public class SiteController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
