958ead6505754f3e5379734ff33ee41fcd8d1cd3
vagaHundred100 <53252321+vagaHundred100@users.noreply.github.com>
2023-02-01 19:10:17 +0400

    Update VariationService.cs

--- ECommerceApp.Services/UserAccountService/Services/Concrete/VariationService.cs
+++ ECommerceApp.Services/UserAccountService/Services/Concrete/VariationService.cs
@@ -1,8 +1,11 @@
-﻿using ECommerceApp.Domain.Entities;
+using AutoMapper;
+using ECommerceApp.Domain.Entities;
 using ECommerceApp.Domain.Repository;
 using ECommerceApp.Infrastructure.DataBase.EntityFramework.EFRepository;
 using ECommerceApp.Services.UserAccountService.DTOs;
 using ECommerceApp.Services.UserAccountService.Services.Abstract;
+using ECommerceApp.Shared.SharedRequestResults.Base;
+using ECommerceApp.Shared.SharedRequestResults.SharedConstants;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,36 +16,60 @@ namespace ECommerceApp.Services.UserAccountService.Services.Concrete
 {
     public class VariationService : IVariationService
     {
+        protected readonly IMapper _mapper;
         protected readonly IVariationRepository _repository;
 
-        public VariationService(IVariationRepository repository)
+        public VariationService(IVariationRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
-        public void Create(VariationCreateDTO variationDTO)
+        public DefaultResult Create(VariationCreateDTO VariationDTO)
         {
-            throw new NotImplementedException();
+            var Variation = _mapper.Map<Variation>(VariationDTO);
+            _repository.Create(Variation);
+            return new DefaultResult();
         }
 
-        public void Delete(int id)
+        public DefaultResult Delete(int id)
         {
-            throw new NotImplementedException();
+            var Variation = _repository.FindById(id);
+
+            if (Variation == null)
+            {
+                throw new KeyNotFoundException(RequestResults.DataNotFound);
+            }
+            _repository.Delete(Variation);
+            return new DefaultResult();
         }
 
-        public Variation Get(int id)
+        public DataResult<Variation> Get(int id)
         {
-            throw new NotImplementedException();
+            var vt = _repository.FindById(id);
+            return new DataResult<Variation>(vt);
         }
 
-        public Variation GetAll()
+        public DataResult<List<Variation>> GetAll(int categoryTypeId)
         {
-            throw new NotImplementedException();
+            var vtList = _repository.FindByCondition(c => c.CategoryTypeId == categoryTypeId).ToList();
+            return new DataResult<List<Variation>>(vtList);
         }
 
-        public void Update(VariationUpdateDTO updateDTO)
+        public DefaultResult Update(VariationUpdateDTO updateDTO)
         {
-            throw new NotImplementedException();
+            var vt = _repository.FindById(updateDTO.Id);
+
+            if (vt == null)
+            {
+                throw new KeyNotFoundException(RequestResults.DataNotFound);
+            }
+
+            _mapper.Map<VariationUpdateDTO, Variation>(updateDTO, vt);
+            _repository.Update(vt);
+
+            return new DefaultResult();
         }
     }
 }
+
